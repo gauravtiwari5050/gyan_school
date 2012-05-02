3ffdaea200faf7677a544c7e06c3d113dead7edd
gt5050 <gaurav2gaurav@gmail.com>
2012-05-02 18:07:00 +0530

    session functionality

--- gyan_school/app/controllers/home_controller.rb
+++ gyan_school/app/controllers/home_controller.rb
@@ -5,9 +5,11 @@ class HomeController < ApplicationController
     @institute_session =  InstituteSession.new
   end
   def session_create
+    logger.info params.inspect
     @institute_session = InstituteSession.new(params[:institute_session])
     logger.info @institute_session.inspect
     @institute_session.institute_id =  get_institute_id
+
     persist_success = @institute_session.save
     respond_to do |format|
       if persist_success
@@ -21,7 +23,50 @@ class HomeController < ApplicationController
 
   def session_index
     @institute = Institute.find_by_id(get_institute_id)
+  end
 
+  def session_edit
+    @institute = Institute.find_by_id(get_institute_id)
+    @institute_session = InstituteSession.find_by_id(params[:session_id])
+    if @institute_session.institute_id != @institute.id
+      flash[:error] = 'You do not have permission to view the page requested'
+      redirect_to(get_home_for_user)
+    end
+    
+  end
+
+  def session_update
+    @institute = Institute.find_by_id(get_institute_id)
+    @institute_session = InstituteSession.find_by_id(params[:session_id])
+    if @institute_session.institute_id != @institute.id
+      flash[:error] = 'You do not have permission to view the page requested'
+      redirect_to(get_home_for_user)
+    end
+
+    respond_to do |format|
+      if @institute_session.update_attributes(params[:institute_session])
+        format.html {redirect_to('/sessions/index')}
+      else
+        format.html {render :action => "session_edit"}
+        
+      end
+    end
+    
+  end
+
+  def session_delete
+    @institute = Institute.find_by_id(get_institute_id)
+    @institute_session = InstituteSession.find_by_id(params[:session_id])
+    if @institute_session.institute_id != @institute.id
+      flash[:error] = 'You do not have permission to view the page requested'
+      redirect_to(get_home_for_user)
+    end
+    @institute_session.destroy #TODO check if this is actualy destroyed
+    respond_to do |format|
+      format.html {redirect_to('/sessions/index')}
+    end
+
+    
   end
   
 end
--- gyan_school/config/routes.rb
+++ gyan_school/config/routes.rb
@@ -7,7 +7,10 @@ GyanSchool::Application.routes.draw do
   match 'login/student' => 'login#login_student' ,:via => :post
   match 'home' => 'home#home'
   match 'session/new' => 'home#session_new'
-  match 'session/index' => 'home#session_index'
+  match 'session/:session_id/edit' => 'home#session_edit'
+  match 'session/:session_id/update' => 'home#session_update',:via =>:post
+  match 'session/:session_id/delete' => 'home#session_delete'
+  match 'sessions/index' => 'home#session_index'
   match 'session/create' => 'home#session_create' ,:via => :post
 
 end
