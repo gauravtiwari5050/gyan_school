883d352dd761cfc8f641625050beb0985480db46
gt5050 <gaurav2gaurav@gmail.com>
2012-05-08 01:15:13 +0530

    removing vendor bundle from git

--- vendor/bundle/ruby/1.9.1/gems/rscribd-1.3.0/spec/error_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'spec_helper'
-
-describe Scribd::ResponseError do
-  it "should set the code attribute on initialization" do
-    error = Scribd::ResponseError.new(123)
-    error.code.should eql(123)
-  end
-end
