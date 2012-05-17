faa5e76e018a6d793e452041854c8e31e00cb3ac
gt5050 <gaurav2gaurav@gmail.com>
2012-05-17 19:46:33 +0530

    remaining screens to newtheme

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -73,6 +73,7 @@ class HomeController < ApplicationController
 
   def batch_new
     @batch = Batch.new
+    @display_title = "Create new class/batch and sections"
     3.times {@batch.sections.build}
   end
 
@@ -93,6 +94,7 @@ class HomeController < ApplicationController
   def batch_edit
     @institute = Institute.find_by_id(get_institute_id)
     @batch = Batch.find_by_id(params[:batch_id])
+    @display_title = "Edit batch/create or remove sections"
     respond_to do |format|
       format.html {render :action => "batch_new"}
     end
@@ -112,7 +114,7 @@ class HomeController < ApplicationController
     @batch = Batch.find_by_id(params[:batch_id])
     respond_to do |format|
       if @batch.update_attributes(params[:batch])
-        format.html {redirect_to('/batch/index')}
+        format.html {redirect_to('/batch/'+@batch.id.to_s+'/show')}
       else
         format.html {render :action => "batch_new"}
         
@@ -236,46 +238,9 @@ class HomeController < ApplicationController
   end
 
 
-  def section_show
-    @section = Section.find_by_id(params[:section_id])
-  end
 
-  def section_attendance_home
-    @section = Section.find_by_id(params[:section_id])
-  end
 
-  def section_mark_attendance
-    @section = Section.find_by_id(params[:section_id])
-    @date = Date.strptime(params[:date],"%Y-%m-%d")
-  end
 
-  def section_mark_attendance_update
-    @section = Section.find_by_id(params[:section_id])
-    @date = Date.strptime(params[:date],"%Y-%m-%d")
-
-    for student in @section.students
-      if params.has_key?(student.id.to_s) 
-         section_attendance  = SectionAttendance.find(:first,:conditions => {:date => params[:date],:section_id => @section.id,:user_id => student.id})
-         if section_attendance.nil?
-           section_attendance = SectionAttendance.new
-           section_attendance.section_id = @section.id
-           section_attendance.user_id = student.id
-           section_attendance.date = params[:date]
-           section_attendance.save #TODO failure check
-         end
-      else
-         section_attendance  = SectionAttendance.find(:first,:conditions => {:date => params[:date],:section_id => @section.id,:user_id => student.id})
-         if !section_attendance.nil?
-          section_attendance.destroy #failure check
-         end
-      end
-
-    end
-    respond_to do |format|
-        format.html { redirect_to('/section/' + @section.id.to_s + '/mark_attendance/'+params[:date], :notice => 'Attendance updated') }
-    end
-    
-  end
 
   def fees_schedule_index
     @institute = Institute.find_by_id(get_institute_id)
@@ -368,41 +333,9 @@ class HomeController < ApplicationController
     end
   end
 
-  def section_exams_index
-    @section = Section.find_by_id(params[:section_id])
-    @exam = Exam.new
-    
-  end
 
-  def section_exams_create
-    @section = Section.find_by_id(params[:section_id])
-    @exam = Exam.new(params[:exam])
-    @exam.examable_type = 'Section'
-    @exam.examable_id = @section.id
-    persist_success = @exam.save
-    respond_to do |format|
-      if persist_success
-        format.html {redirect_to('/section/' + @section.id.to_s  + '/exams' )}
-      else
-        format.html {render :action => "section_exams_index"}
-      end
-    end
-    
-  end
 
-  def section_exam_subjects
-    @institute = Institute.find_by_id(get_institute_id)
-    @section = Section.find_by_id(params[:section_id])
-    @exam = Exam.find_by_id(params[:exam_id])
-    
-  end
 
-  def section_exam_subject_marks
-    @institute = Institute.find_by_id(get_institute_id)
-    @section = Section.find_by_id(params[:section_id])
-    @exam = Exam.find_by_id(params[:exam_id])
-    @subject = Course.find_by_id(params[:course_id])
-  end
 
   def section_exam_subject_marks_update
     #TODO 1 check for exam result update/save failure and report error in meesage for specific students
